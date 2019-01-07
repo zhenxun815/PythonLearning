6f31ff1c9a9cc97685d5cd3fbd903bcc52ea63f6
zhenxun815 <guoyiheng89@gmail.com>
2019-01-08 00:42:13 +0800

    操作符和表达式

---
+++ test01/TestOperator&Expression.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# 算数运算符
+# **幂,//整除
+# 输出:81
+# print(3**4)
+# 输出 -4
+# print(-13//4)
+
+
+# 逻辑运算符
+# not,and,or
+
+# 优先级,由上到下递减
+# lambda : Lambda Expression
+# if - else : Conditional expression
+# or : Boolean OR
+# and : Boolean AND
+# not x : Boolean NOT
+# in, not in, is, is not, <, <=, >, >=, !=, == : Comparisons, including membership tests and identity tests
+# | : Bitwise OR
+# ^ : Bitwise XOR
+# & : Bitwise AND
+# <<, >> : Shifts
+# +, - : Addition and subtraction
+# *, /, //, % : Multiplication, Division, Floor Division and Remainder
+# +x, -x, ~x : Positive, Negative, bitwise NOT
+# ** : Exponentiation
+# x[index], x[index:index], x(arguments...), x.attribute : Subscription, slicing, call, attribute reference
+# (expressions...), [expressions...], {key: value...}, {expressions...} : Binding or tuple display, list display, dictionary display, set display
