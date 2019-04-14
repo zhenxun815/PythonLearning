15ae7793ef52b56e56c2eb22c695810c81b77b1b
zhenxun815 <guoyiheng89@gmail.com>
2019-04-14 16:26:27 +0800

    pickle

--- test03/io_file.py
+++ test03/io_file.py
@@ -7,3 +7,24 @@
 # @Author: Yiheng
 # @Email: GuoYiheng89@gmail.com
 # @Time: 4/9/19 9:36 PM
+
+text = 'something to write'
+
+f = open('test.txt', 'w')
+f.write(text)
+f.close()
+
+f = open('test.txt')
+while True:
+    line = f.readline()
+    if len(line) == 0:
+        break
+    print(line, end='')
+
+f.close()
+
+# recommend
+print('###################')
+with open('test.txt') as f:
+    for line in f:
+        print(line)
---
+++ test03/io_pickle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Description: 
+# @File: io_pickle.py
+# @Project: PythonLearning
+# @Author: Yiheng
+# @Email: GuoYiheng89@gmail.com
+# @Time: 4/11/19 10:33 PM
+import pickle
+
+shop_list_file = 'shop_list.data'
+shop_list = ['apple', 'mango', 'carrot']
+
+with open(shop_list_file, 'wb') as f:
+    pickle.dump(shop_list, f)
+
+del shop_list
+
+with open(shop_list_file,'rb') as f:
+    store_list= pickle.load(f)
+    print(store_list)
