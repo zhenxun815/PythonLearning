6f1f46646973f9d2f98c6c6fddb03ca762963b98
zhenxun815 <guoyiheng89@gmail.com>
2019-04-14 17:39:48 +0800

    exception handel

---
+++ test04/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Description: test Exception
+# @File: __init__.py.py
+# @Project: PythonLearning
+# @Author: Yiheng
+# @Email: GuoYiheng89@gmail.com
+# @Time: 4/14/19 5:14 PM
---
+++ test04/exception_handle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Description: 
+# @File: exception_handle.py
+# @Project: PythonLearning
+# @Author: Yiheng
+# @Email: GuoYiheng89@gmail.com
+# @Time: 4/14/19 4:58 PM
+
+try:
+    text = input('enter someting...')
+except EOFError:
+    print('catch a EOF!')
+except KeyboardInterrupt:
+    print('user cancelled')
+else:
+    print('user entered {}'.format(text))
